9d8820ec5dfb815196b4a749356e4dc640b05e13
ravi <sumanrajak2602@gmail.com>
2024-12-20 09:28:25 +0530

    initial commit

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://localhost:8080', {
+export const socket = io('https://tutor-9lu8jqh4s-suman-rajaks-projects.vercel.app/', {
   autoConnect: true,
   reconnection: true
 });
