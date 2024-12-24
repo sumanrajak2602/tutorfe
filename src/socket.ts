46586dfdfcf66a3be3670b4dca5441eb0dccb483
ravi <sumanrajak2602@gmail.com>
2024-12-25 00:18:02 +0530

    initial commit

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('https://tutor-9lu8jqh4s-suman-rajaks-projects.vercel.app/', {
+export const socket = io('https://tutor-virid.vercel.app/', {
   autoConnect: true,
   reconnection: true
 });
