3dbc0f7abce1e6cc682de140edc953565938e21d
ravi <sumanrajak2602@gmail.com>
2024-12-28 10:41:31 +0530

    socket

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('https://tutor-virid.vercel.app/', {
+export const socket = io('http://3.111.33.229:8080/', {
   autoConnect: true,
   reconnection: true
 });
