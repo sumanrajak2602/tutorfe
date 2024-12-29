231f9d361058c3a6cdb466d7e3564584dcaebd70
ravi <sumanrajak2602@gmail.com>
2024-12-29 09:19:07 +0530

    socket

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('http://3.111.33.229:8080/', {
+export const socket = io('http://13.201.80.185:8080/', {
   autoConnect: true,
   reconnection: true
 });
